d71e984937fdee7ff94774e51b88db4b3c5c5b46
Your Name <shubhamthoratjuly1999@gmail.com>
2020-02-16 17:28:11 +0530

    with Home

--- app/src/main/java/me/shubhamthorat/rohan/Moreinfo.java
+++ app/src/main/java/me/shubhamthorat/rohan/Moreinfo.java
@@ -2,15 +2,44 @@ package me.shubhamthorat.rohan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 public class Moreinfo extends AppCompatActivity {
+    ViewFlipper v_flipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_moreinfo);
+
+        int image[] ={R.drawable.floor1,R.drawable.floor2,R.drawable.floor3, R.drawable.slide4};
+
+        v_flipper = findViewById(R.id.v_flipper);
+
+        for(int i=0; i < image.length;i++){
+            flipperImage(image[i]);
+        }
+    }
+    public void flipperImage(int image){
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(image);
+
+        v_flipper.addView(imageView);
+        v_flipper.setFlipInterval(4000);
+        v_flipper.setAutoStart(true);
+
+        v_flipper.setInAnimation(this,android.R.anim.slide_in_left);
+        v_flipper.setInAnimation(this,android.R.anim.slide_out_right);
     }
 
+//    public void intent (View view) {
+//
+//        Intent intent = new Intent(MainActivity.this, Moreinfo.class);
+//        startActivity(intent);
+//    }
 
 }
--- app/src/main/java/me/shubhamthorat/rohan/ProfileFragment.java
+++ app/src/main/java/me/shubhamthorat/rohan/ProfileFragment.java
@@ -27,7 +27,7 @@ public class ProfileFragment extends Fragment {
         return fragment;
     }
 
-    public ViewFlipper v_flipper;
+//    public ViewFlipper v_flipper;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,29 +37,29 @@ public class ProfileFragment extends Fragment {
 
 
 
-    public void flipperImage(int image) {
-        ImageView imageView = new ImageView(getActivity());
-        imageView.setBackgroundResource(image);
-
-        v_flipper.addView(imageView);
-        v_flipper.setFlipInterval(2000);
-        v_flipper.setAutoStart(true);
-
-        v_flipper.setInAnimation(getActivity(), android.R.anim.slide_in_left);
-        v_flipper.setInAnimation(getActivity(), android.R.anim.slide_out_right);
-    }
+//    public void flipperImage(int image) {
+//        ImageView imageView = new ImageView(getActivity());
+//        imageView.setBackgroundResource(image);
+//
+//        v_flipper.addView(imageView);
+//        v_flipper.setFlipInterval(2000);
+//        v_flipper.setAutoStart(true);
+//
+//        v_flipper.setInAnimation(getActivity(), android.R.anim.slide_in_left);
+//        v_flipper.setInAnimation(getActivity(), android.R.anim.slide_out_right);
+//    }
 
 
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        int image[] = {R.drawable.slide1, R.drawable.slide2, R.drawable.slide3, R.drawable.logo};
-        v_flipper = view.findViewById(R.id.v_flipper);
-        if (!v_flipper.isFlipping())
-            v_flipper.startFlipping();
-        for (int i = 0; i < image.length; i++) {
-            flipperImage(image[i]);
+//        int image[] = {R.drawable.slide1, R.drawable.slide2, R.drawable.slide3, R.drawable.logo};
+//        v_flipper = view.findViewById(R.id.v_flipper);
+//        if (!v_flipper.isFlipping())
+//            v_flipper.startFlipping();
+//        for (int i = 0; i < image.length; i++) {
+//            flipperImage(image[i]);
         }
 //        TextView textView = view.findViewById(R.id.textView4);
 //        textView.setOnClickListener(new View.OnClickListener() {
@@ -69,7 +69,7 @@ public class ProfileFragment extends Fragment {
 ////        startActivity(intent);
 //            }
 //        });
-    }
+ //   }
 
     @Override
     public void onResume() {
